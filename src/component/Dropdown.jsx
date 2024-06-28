a43068e24177a9904c87013a4219843eac9b8d5b
Ieremia Andronic <contact.ierem@gmail.com>
2024-06-28 22:49:37 +0200

    cleaning dropdown

--- src/component/Dropdown.jsx
+++ src/component/Dropdown.jsx
@@ -1,18 +1,14 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export function Dropdown({ title, children, className = "" }) {
   const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
     setIsOpen((prev) => !prev);
   };
 
   return (
-    <div
-      ref={dropdownRef}
-      className={`dropdowns ${isOpen ? "open" : ""} ${className}`}
-    >
+    <div className={`dropdowns ${isOpen ? "open" : ""} ${className}`}>
       <h3 onClick={toggleDropdown}>
         {title}
         <i class="fa-solid fa-chevron-up"></i>
